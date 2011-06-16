6ffa2782089a3f37bff5abb9a9d78324c00c6710
Doug Bridgens <doug.bridgens@soogate.com>
2011-06-16 22:39:09 +0100

    Revert "conforming to perl rule 1, use strict"
    This reverts commit 76a398b6db5dbe3ce2d0816b7b99988242b66fbd.

--- viewRadar.pl
+++ viewRadar.pl
@@ -23,10 +23,9 @@
 # log_transaction
 # uri
 
-use Data::Dumper;
 use DB_File qw($DB_HASH);
+use Data::Dumper;
 use Fcntl;
-use strict;
 
 my $db_file = "/tmp/radar.db";
 my $db_ref;
@@ -36,17 +35,17 @@ my $requests;
 my $slowrequests;
 
 if (tie(%db, 'DB_File', $db_file, O_RDONLY)) {
-  for my $pid (keys %db) {
-    my $tmp = $db{$pid};
+  for $pid (keys %db) {
+    $tmp = $db{$pid};
     $tmp =~ s/\s+//g;
     my ($client_ip, $time_started, $post_read, $translate_name, $map_to_storage, $header_parser, $access_checker, $check_user_id, $auth_checker, $type_checker, $fixups, $log_transaction, $uri) = split(/\,/, $tmp);
 
       $requests++;
-      my $runfor = int($time_started / 1000000);
+      $runfor = int($time_started / 1000000);
       $runfor = time() - $runfor;
       if ($runfor > 10) { $slowrequests++; }
       $fixups = ($fixups / 1000000);
-      my $row = sprintf("%-8d  %15s  %11.3f  %10d  %s\n", $pid, $client_ip, $fixups, $runfor, $uri);
+      $row = sprintf("%-8d  %15s  %11.3f  %10d  %s\n", $pid, $client_ip, $fixups, $runfor, $uri);
       $rows{$runfor} = $row;
     }
 }
@@ -54,6 +53,6 @@ untie %db;
 
 printf(":: mod_radar ::\n\n%-8s  %15s  %11s  %10s  %s\n", "pid", "client ip", "pre-process", "processing", "uri");
 
-foreach my $r (sort { $b <=> $a} keys %rows) {
+foreach $r (sort { $b <=> $a} keys %rows) {
   print $rows{$r};
 }
